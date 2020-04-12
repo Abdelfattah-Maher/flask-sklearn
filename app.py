30e7b79bfdd52c0058ba030c88c546ae90a47659
Abdelfattah M <abdelfatah.maher@gmail.com>
2020-04-12 19:08:16 +0200

    Rename Developer x

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ def predict():
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
     LOG.info(f"prediction {prediction}")
-    return jsonify({'prediction': prediction})
+    name = 'Developer X.'
+    return jsonify({'Author':name, 'prediction': prediction})
 
 if __name__ == "__main__":
     clf = joblib.load("boston_housing_prediction.joblib")
