99e02334b947ca50034532d56938f03174b620fb
Abdelfattah M <abdelfatah.maher@gmail.com>
2020-04-12 10:39:53 +0200

    deploy to kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,17 +4,21 @@
 
 # Step 1:
 # This is your Docker ID
-#dockerpath=""
+dockerpath="flask-sklearn"
 
 # Step 2
 # Run in Docker Hub container with kubernetes
-
+kubectl run house-predicition\
+    --generator=run-pod/v1
+    --image=$dockerpath
+    --port=80 --labels app=house-predicition
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to host
-
+kubectl port-forward house-predicition 8000:80
 
 
